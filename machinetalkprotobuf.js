85d11b18c8a4e47ce734faa51c6f64c6973a45fe
Bob van der Linden <bobvanderlinden@gmail.com>
2015-09-11 00:09:15 +0200

    find protobuf files by searching common directories

--- machinetalkprotobuf.js
+++ machinetalkprotobuf.js
@@ -1,8 +1,24 @@
 var ProtoBuf = require('protobufjs');
+var path = require('path');
+var fs = require('fs');
+var child_process = require('child_process');
+
+function findDirectory(directories, file) {
+	return directories
+		.filter(function(directoryPath) { return !!directoryPath; })
+		.filter(function(directoryPath) { return fs.existsSync(path.join(directoryPath, file)); })
+		[0];
+}
 
 var builder = ProtoBuf.loadProtoFile({
-	// TODO: Search in /usr/include, /usr/local/include, etc for machinetalk-protobuf
-	root: '/home/bob/projects/machinetalk-protobuf/src',
+	root: findDirectory([
+		process.env.PROTOBUF_INCLUDE,
+		child_process.execSync('pkg-config --variable=includedir protobuf').toString(),
+		'/home/bob/projects/machinetalk-protobuf/src',
+		'/home/bob/projects/machinetalk-protobuf/proto',
+		'/usr/include',
+		'/usr/local/include',
+	], 'machinetalk/protobuf/message.proto'),
 	file: 'machinetalk/protobuf/message.proto'
 });
 
