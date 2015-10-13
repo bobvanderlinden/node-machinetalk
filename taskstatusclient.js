68710fb5837cbfe47224ad103ce55a88fa554cdd
Bob van der Linden <bobvanderlinden@gmail.com>
2015-10-13 21:17:06 +0200

    removed unused taskTopic

--- taskstatusclient.js
+++ taskstatusclient.js
@@ -5,6 +5,4 @@ function TaskStatusClient(address) {
   StatusClient.call(this, address, 'task');
 }
 util.inherits(TaskStatusClient, StatusClient);
-TaskStatusClient.taskTopic = 'task';
-
 module.exports = TaskStatusClient;
